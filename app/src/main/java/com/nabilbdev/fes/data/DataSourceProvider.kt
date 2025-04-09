fb2f651fdca4a074bdafc0d358e73a11664d4a57
CPhuoc <phuocmc.23itb@vku.udn.vn>
2025-04-09 14:39:24 +0700

    update

--- app/src/main/java/com/nabilbdev/fes/data/DataSourceProvider.kt
+++ app/src/main/java/com/nabilbdev/fes/data/DataSourceProvider.kt
@@ -5,7 +5,6 @@ import com.nabilbdev.fes.data.model.CategoryOptions
 import com.nabilbdev.fes.data.model.Recommendation
 import com.nabilbdev.fes.ui.utils.FesPlacesReview
 
-/** Map of available CategoryItem to be displayed in Category Fragments*/
 object DataSourceProvider {
 
     val defaultRecommendation: Recommendation = Recommendation(
@@ -18,98 +17,98 @@ object DataSourceProvider {
 
     val allRecommendations = listOf(
         Recommendation(
-            name = "Al-Qarawiyyin University",
-            description = "The University of al-Qarawiyyin is one of the oldest universities in the world. It was founded over a thousand years ago, in 859 AD, by a woman named Fatima al-Fihri. The university has a rich history and is known for its Islamic studies and Arabic language.",
+            name = "Bà Nà Hills",
+            description = "Đà Nẵng với danh xưng thành phố đáng sống nhất Việt Nam ắt hẳn là có lý do của nó. Nằm ở trung tâm khu vực duyên hải miền Trung, Đà Nẵng đã, đang và sẽ luôn là điểm hết hàng đầu của du khách trong những chuyến du lịch Việt Nam.",
             review = FesPlacesReview.FIVE,
             categoryOptions = CategoryOptions.LANDMARKS,
             imageResourceId = R.drawable.land1
         ),
 
         Recommendation(
-            name = "Madrasa al-Attarine",
-            description = "The Al-Attarine Madrasawas built over 600 years ago as a school for students to learn about Islam and other subjects. Today, the madrasa is not used as a school anymore but it is open to visitors who can explore its beautiful architecture.",
+            name = "Fansipan",
+            description = "Tại sao du lịch Việt Nam lại không nên bỏ qua Sapa? Đơn giản bởi nếu ghé Sapa, nó sẽ đưa bạn hòa mình vào  giữa những thửa ruộng bậc thang chín vàng rực rỡ, trải dài bất tận như dải lụa óng ánh mềm mại quấn quanh sườn núi. Đó là khoảnh khắc đẹp nhất của Sapa khi mùa lúa chín nhuộm vàng mọi ngõ ngách, biến nơi đây thành một bức tranh thiên nhiên kỳ vĩ.",
             review = FesPlacesReview.FOUR,
             categoryOptions = CategoryOptions.LANDMARKS,
             imageResourceId = R.drawable.land2
         ),
 
         Recommendation(
-            name = "Bab Ftouh",
-            description = "Bab Ftouh is the main southeastern gate of Fes el-Bali, the old walled city of Fes, Morocco.",
+            name = "Thác Bản Giốc",
+            description = "Đến thác Bản Giốc, bạn sẽ bị choáng ngợp bởi vẻ đẹp hoang sơ và âm thanh rì rào của nước chảy.",
             review = FesPlacesReview.THREE,
             categoryOptions = CategoryOptions.LANDMARKS,
             imageResourceId = R.drawable.land3
         ),
 
         Recommendation(
-            name = "Al-Qarawiyyin University",
-            description = "The University of al-Qarawiyyin is one of the oldest universities in the world. It was founded over a thousand years ago, in 859 AD, by a woman named Fatima al-Fihri. The university has a rich history and is known for its Islamic studies and Arabic language programs.",
+            name = "Quốc Tử Giám",
+            description = " Hà Nội là cầu Thê Húc màu đỏ rực, là Lăng Bác thiêng liêng, là Quốc Tử Giám xuất hiện trên những trang sách.",
             review = FesPlacesReview.FIVE,
             categoryOptions = CategoryOptions.LANDMARKS,
-            imageResourceId = R.drawable.land1
+            imageResourceId = R.drawable.land4
         ),
 
         Recommendation(
-            name = "Madrasa al-Attarine",
-            description = "The Al-Attarine Madrasawas built over 600 years ago as a school for students to learn about Islam and other subjects. Today, the madrasa is not used as a school anymore but it is open to visitors who can explore its beautiful architecture.",
+            name = "Hội An",
+            description = "Phố cổ Hội An luôn khiến lòng người mê mẩn trong chuyến du lịch Việt Nam bởi nét đẹp dung dị mà không kém phần quyến rũ như một bức tranh thời gian ngưng đọng. Vẻ cổ kính hòa quyện với sự thơ mộng nơi đây đã biến Hội An trở thành một trong những phố cổ đẹp nhất châu Á.",
             review = FesPlacesReview.FOUR,
             categoryOptions = CategoryOptions.LANDMARKS,
-            imageResourceId = R.drawable.land2
+            imageResourceId = R.drawable.land5
         ),
 
         Recommendation(
-            name = "Bab Ftouh",
-            description = "Bab Ftouh is the main southeastern gate of Fes el-Bali, the old walled city of Fes, Morocco.",
+            name = "Cố đô Huế",
+            description = "Cố đô Huế là một trong những điểm đến nổi bật trong hành trình du lịch Việt Nam. Nổi tiếng với những công trình kiến trúc lịch sử như Đại Nội hay lăng tẩm của các vua Nguyễn, Huế mang đến cho du khách những trải nghiệm độc đáo về văn hóa và lịch sử.",
             review = FesPlacesReview.THREE,
             categoryOptions = CategoryOptions.LANDMARKS,
-            imageResourceId = R.drawable.land3
+            imageResourceId = R.drawable.land6
         ),
 
         Recommendation(
-            name = "Cafe Clock",
-            description = " Good luck finding this magnificently restored house in the old medina turned into a cafe. Look for the sign or ask a shopkeeper. The people are super friendly (and speak English) and the food is excellent. Service can be slow.",
+            name = "Nhà Hàng Veranda Vietnamese",
+            description = "Đến với nhà hàng Veranda Vietnamese, thực khách sẽ được thưởng ngoạn vẻ đẹp và hương vị tinh tế của ẩm thực Việt 3 miền do chính bếp trưởng Luis Thạch tuyển chọn và chế tác tỉ mỉ.",
             review = FesPlacesReview.FOUR,
             categoryOptions = CategoryOptions.RESTAURANTS,
             imageResourceId = R.drawable.restaurant1
         ),
 
         Recommendation(
-            name = "La Kasbah Restaurant",
-            description = "Friendly service, a solid selection of inexpensive Moroccan staples (excellent vegetarian tagine) and a couple of lovely high terraces overlooking the Gate on one side and the medina on the other.",
+            name = "Steak & Seafood Buffet - Furama Resort Da Nang",
+            description = "Furama Resort Da Nang cũng là một cái tên chuyên về buffet hải sản được cả người dân địa phương lẫn du khách thập phương yêu thích.",
             review = FesPlacesReview.THREE,
             categoryOptions = CategoryOptions.RESTAURANTS,
             imageResourceId = R.drawable.restaurant2
         ),
 
         Recommendation(
-            name = "Restaurant DAR SAADA",
-            description = "Located in the centre of the medina, this restaurant is a favorite of Travel and Leisure magazine and is worth the indulgence.",
+            name = "Golden Dragon Restaurant",
+            description = "Khách sạn Grand Mercure không chỉ đem tới dịch vụ lưu trú nghỉ dưỡng cao cấp, mà nơi này còn có đa dạng nhà hàng phục vụ tinh hoa ẩm thực trong và ngoài nước cho bạn tha hồ lựa chọn.",
             review = FesPlacesReview.ONE,
             categoryOptions = CategoryOptions.RESTAURANTS,
             imageResourceId = R.drawable.restaurant3
         ),
         Recommendation(
-            name = "Cafe Clock",
-            description = " Good luck finding this magnificently restored house in the old medina turned into a cafe. Look for the sign or ask a shopkeeper. The people are super friendly (and speak English) and the food is excellent. Service can be slow.",
+            name = "Nhà Hàng All Seasons",
+            description = "Nhà hàng All Seasons là một cái tên “must-try” mà bạn nhất định phải ghé đến khi đến với thành phố Đà Nẵng.",
             review = FesPlacesReview.FOUR,
             categoryOptions = CategoryOptions.RESTAURANTS,
-            imageResourceId = R.drawable.restaurant1
+            imageResourceId = R.drawable.restaurant4
         ),
 
         Recommendation(
-            name = "La Kasbah Restaurant",
-            description = "Friendly service, a solid selection of inexpensive Moroccan staples (excellent vegetarian tagine) and a couple of lovely high terraces overlooking the Gate on one side and the medina on the other.",
+            name = "Nhà Hàng The Temptation",
+            description = "Nhà hàng Pháp tiếp theo tại Đà Nẵng mà #teamKlook không nên bỏ qua là nhà hàng The Temptation, khi mỗi món ăn là sự thăng hoa của thị giác và cảm xúc về nước Pháp thơ mộng.",
             review = FesPlacesReview.THREE,
             categoryOptions = CategoryOptions.RESTAURANTS,
-            imageResourceId = R.drawable.restaurant2
+            imageResourceId = R.drawable.restaurant5
         ),
 
         Recommendation(
-            name = "Restaurant DAR SAADA",
-            description = "Located in the centre of the medina, this restaurant is a favorite of Travel and Leisure magazine and is worth the indulgence.",
+            name = "Nhà Hàng Nêm",
+            description = "Nhà hàng Nêm là điểm đến lý tưởng và quen thuộc cho nhiều tín đồ muốn được thưởng thức ẩm thực Việt Nam truyền thống.",
             review = FesPlacesReview.ONE,
             categoryOptions = CategoryOptions.RESTAURANTS,
-            imageResourceId = R.drawable.restaurant3
+            imageResourceId = R.drawable.restaurant6
         ),
 
         Recommendation(
